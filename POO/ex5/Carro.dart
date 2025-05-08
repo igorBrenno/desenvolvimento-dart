fcde59634c9c7122124e0404ed3c97809c6ac434
Igor Brenno Dantas <54316405+igorBrenno@users.noreply.github.com>
2025-05-08 18:24:41 +0000

    exercicio

---
+++ POO/ex5/Carro.dart
@@ -0,0 +1,33 @@
+class Carro {
+  String _marca = "";
+  String _modelo = "";
+  int _ano = 0;
+
+  Carro(this._marca, this._modelo, this._ano);
+
+  String get marca => _marca;
+  String get modelo => _modelo;
+  int get ano => _ano;
+
+  set marca(String marca) {
+    _marca = marca;
+  }
+
+  set modelo(String modelo) {
+    _modelo = modelo;
+  }
+
+  set ano(int ano) {
+    _ano = ano;
+  }
+
+  void CalcularIdade() {
+    int calc = 2025 - _ano;
+    print("A idade do carro é $calc");
+  }
+
+  @override
+  String toString() {
+    return "Marca do carro: $_marca\nModelo do Carro: $_modelo\nO Ano do carro é: $_ano";
+  }
+}
---
+++ POO/ex5/Q5.dart
@@ -0,0 +1,7 @@
+import 'Carro.dart';
+
+void main() {
+  Carro c1 = Carro("Honda", "Civic", 2015);
+
+  print(c1.toString());
+}
